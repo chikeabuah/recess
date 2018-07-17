184ced4ff59e2a0a4141e7735f0499ebbb7234bf
Chike Abuah <abuahchu@gmail.com>
2018-07-16 23:43:44 -0400

    create define-system macro outline

--- examples/tetris/recess.rkt
+++ examples/tetris/recess.rkt
@@ -6,7 +6,7 @@
  (all-defined-out)
  (all-from-out racket))
 
-(define recess-graph (unweighted-graph/directed '(a)))
+(define recess-graph (unweighted-graph/directed '()))
 (define-vertex-property recess-graph archetype)
 (define-vertex-property recess-graph map-fn)
 (define-vertex-property recess-graph parents #:init '())
@@ -18,45 +18,111 @@
 ;; Seems that components may be lambdas 
 (define-syntax (define-component stx)
   (syntax-parse stx
-   [(_ name [~optional body])
-    #''(name (~? body '()))]))
+    [(_ name [~optional body])
+     #''(name (~? body '()))]))
 
 
 (define-syntax (define-archetype stx)
   (syntax-parse stx
-   [(_ (name [arg-id default-expr] ...) components ...)
-    #''(define name
-         (lambda ([arg-id default-expr] ...)
-           (components ...)))]))
+    [(_ (name [arg-id default-expr] ...) components ...)
+     #''(define name
+          (lambda ([arg-id default-expr] ...)
+            (components ...)))]))
 
 ;; An event is an identifier [a predicate and an expression]
 (define-syntax (define-event stx)
   (syntax-parse stx
-   [(_ name [~optional pred] [~optional body])
-    #''(name (~? pred '()) (~? body '()))]))
+    [(_ name [~optional pred] [~optional body])
+     #''(name (~? pred '()) (~? body '()))]))
+
+(define-syntax (define-system stx)
+  (syntax-parse stx
+    [(_ system-name (~optional (~seq #:archetype archetype-name))
+        (~optional (~seq #:on new-inputs))
+        (~optional (~seq #:out new-outputs))
+        (~optional (~seq #:depends new-dependencies))
+        (~optional (~seq #:map map-fn)))
+     #'(begin
+          (add-vertex! recess-graph 'system-name)
+          (for-each (lambda (parent)
+                      (begin
+                        (add-vertex! recess-graph 'system-name)
+                        (add-directed-edge! recess-graph parent 'system-name)
+                        )
+                      )
+                    (~? new-dependencies '()))
+          (and (~? archetype-name #f) (archetype-set! 'system-name (~? archetype-name #f)))
+          (and (~? map-fn #f) (map-fn-set! 'system-name (~? map-fn #f)))
+          (and
+           (~? new-inputs #f)
+           (inputs-set! 'system-name (cons (~? new-inputs #f) (inputs 'system-name #:default '()))))
+          (and
+           (~? new-outputs #f)
+           (outputs-set! 'system-name (cons (~? new-outputs #f) (outputs 'system-name #:default '()))))
+          (and
+           (~? new-dependencies #f)
+           (parents-set! 'system-name (cons (~? new-dependencies #f) (parents 'system-name #:default '()))))
+          (display (graphviz recess-graph)))
+     ]))
 
 
-(define define-system
-  (lambda
-      (system-name
-       #:archetype [archetype-name #f]
-       #:on [new-inputs #f]
-       #:out [new-outputs #f]
-       #:depends [new-dependencies #f]
-       #:map [map-fn #f])
-    (begin
-      (add-vertex! recess-graph system-name)
-      (and archetype-name (archetype-set! system-name archetype-name))
-      (and map-fn (map-fn-set! system-name map-fn))
-      (and
-       new-inputs
-       (inputs-set! system-name (cons new-inputs (inputs system-name #:default '()))))
-      (and
-       new-outputs
-       (outputs-set! system-name (cons new-outputs (outputs system-name #:default '()))))
-      (and
-       new-dependencies
-       (parents-set! system-name (cons new-dependencies (parents system-name #:default '()))))
-      (graphviz recess-graph))))
+;     #''(lambda
+;            (system-name
+;             #:archetype [archetype-name (~? p-archetype)]
+;             #:on [new-inputs (~? p-inputs)]
+;             #:out [new-outputs (~? p-outputs)]
+;             #:depends [new-dependencies (~? p-dependencies)]
+;             #:map [map-fn (~? p-map-func)])
+;          (begin
+;            (add-vertex! recess-graph system-name)
+;            (for-each (lambda (parent)
+;                        (begin
+;                          (add-vertex! recess-graph system-name)
+;                          (add-directed-edge! recess-graph parent system-name)
+;                          )
+;                        )
+;                      new-dependencies)
+;            (and archetype-name (archetype-set! system-name archetype-name))
+;            (and map-fn (map-fn-set! system-name map-fn))
+;            (and
+;             new-inputs
+;             (inputs-set! system-name (cons new-inputs (inputs system-name #:default '()))))
+;            (and
+;             new-outputs
+;             (outputs-set! system-name (cons new-outputs (outputs system-name #:default '()))))
+;            (and
+;             new-dependencies
+;             (parents-set! system-name (cons new-dependencies (parents system-name #:default '()))))
+;            (graphviz recess-graph)))
+
+;(define define-system
+;  (lambda
+;      (system-name
+;       #:archetype [archetype-name #f]
+;       #:on [new-inputs #f]
+;       #:out [new-outputs #f]
+;       #:depends [new-dependencies #f]
+;       #:map [map-fn #f])
+;    (begin
+;      (add-vertex! recess-graph system-name)
+;      (for-each (lambda (parent)
+;                  (begin
+;                    (add-vertex! recess-graph system-name)
+;                    (add-directed-edge! recess-graph parent system-name)
+;                    )
+;                  )
+;                new-dependencies)
+;      (and archetype-name (archetype-set! system-name archetype-name))
+;      (and map-fn (map-fn-set! system-name map-fn))
+;      (and
+;       new-inputs
+;       (inputs-set! system-name (cons new-inputs (inputs system-name #:default '()))))
+;      (and
+;       new-outputs
+;       (outputs-set! system-name (cons new-outputs (outputs system-name #:default '()))))
+;      (and
+;       new-dependencies
+;       (parents-set! system-name (cons new-dependencies (parents system-name #:default '()))))
+;      (graphviz recess-graph))))
 
 
--- examples/tetris/tetris.rkt
+++ examples/tetris/tetris.rkt
@@ -55,145 +55,141 @@
 
 ;; XXX Systems
 
-(define-system 'handle-input    
+(define-system handle-input    
   #:out 'key-event)    
 
-(define-system 'compute-collision-structure    
+(define-system compute-collision-structure    
   #:archetype 'Block
   #:on '(touched-bottom? collision-structure)
-  #:out 'collision-structure
-  #:depends 'tetro-to-blocks
+  #:out '(collision-structure)
+  #:depends '(tetro-to-blocks)
   #:map (lambda (e) 
     (vector-set! 
       (vector-ref 'collision-structure 'e.Position.y) 
       'e.Position.x
       #t)))        
 
-;(define-system touched-bottom?    
-;  #:archetype ActiveTetromino
-;  #:on clock-tick collision-structure
-;  #:out touched-bottom?
-;  #:map (lambda (e)
-;    (vector-ref 
-;      (vector-ref collision-structure (sub1 e.Position.y)) 
-;      e.Position.x)))
-;  
-;
-;(define-system check-block-overflow   
-;  #:archetype ActiveTetromino
-;  #:on collision-structure
-;  #:out touched-bottom?
-;  #:depends compute-collision-structure
-;  #:map (lambda (e) (< e.Position.y 0)))
-;
-;
-;(define-system tetros-to-blocks   
-;  #:archetype ActiveTetromino
-;  #:on touched-bottom?
-;  #:depends move-down
-;  #:map (lambda (e) (tetro-to-blocks e)))
-;
-;
+(define-system touched-bottom?    
+  #:archetype 'ActiveTetromino
+  #:on '(clock-tick collision-structure)
+  #:out '(touched-bottom?)
+  #:map (lambda (e)
+    (vector-ref 
+      (vector-ref 'collision-structure (sub1 'e.Position.y)) 
+      'e.Position.x)))
+
+(define-system check-block-overflow   
+  #:archetype 'ActiveTetromino
+  #:on '(collision-structure)
+  #:out '(touched-bottom?)
+  #:depends '(compute-collision-structure)
+  #:map (lambda (e) (< 'e.Position.y 0)))
+
+(define-system tetros-to-blocks   
+  #:archetype 'ActiveTetromino
+  #:on '(touched-bottom?)
+  #:depends '(move-down)
+  #:map (lambda (e) ('tetro-to-blocks e)))
+
 ;(define-system clear-full-rows
-;  #:on collision-structure touched-bottom?
-;  #:depends compute-collision-structure
-;  #:map collision-structure (lambda (e) 
+;  #:on '(collision-structure touched-bottom?)
+;  #:depends '(compute-collision-structure)
+;  #:map (lambda (e) 
 ;    (if (apply and e) (set! e (make-vector COLS #f)))))
 ;
 ;
 ;(define-system increment-timer
 ;  #:archetype Timer    
-;  #:on clock-tick
+;  #:on '(clock-tick)
 ;  #:map (lambda (e) (set! e.Timer.val (add1 e.Timer.val))))
 ;
 ;
 ;(define-system increment-score
 ;  #:archetype Score  
-;  #:on touched-bottom?
+;  #:on '(touched-bottom?)
 ;  #:map (lambda (e) (set! e.Score.val (add1 e.Score.val))))
 ;
 ;
 ;(define-system hard-drop    
 ;  #:archetype ActiveTetromino
-;  #:on (eq? key-event 'down) collision-structure
-;  #:depends compute-collision-structure move-down
+;  #:on '((eq? key-event 'down) collision-structure)
+;  #:depends '(compute-collision-structure move-down)
 ;  #:map (lambda (e) (set! e.Position.y (lowest-y e collision-structure))))
 ;
 ;
 ;(define-system soft-drop    
 ;  #:archetype ActiveTetromino
-;  #:on (eq? key-event 'd) collision-structure
-;  #:depends compute-collision-structure move-down
+;  #:on '((eq? key-event 'd) collision-structure)
+;  #:depends '(compute-collision-structure move-down)
 ;  #:map (lambda (e) (set! e.Position.y (- e.Position.y 3))))
 ;
 ;
 ;(define-system rotate-ccw    
 ;  #:archetype ActiveTetromino
-;  #:on (eq? can-rotate-ccw? #t) collision-structure
-;  #:depends compute-collision-structure
+;  #:on '((eq? can-rotate-ccw? #t) collision-structure)
+;  #:depends '(compute-collision-structure)
 ;  #:map (lambda (e) (rotate90 (rotate90 (rotate90 e)))))
 ;
 ;
 ;(define-system rotate-cw    
 ;  #:archetype ActiveTetromino
-;  #:on (eq? can-rotate-cw? #t) collision-structure
-;  #:depends compute-collision-structure
+;  #:on '((eq? can-rotate-cw? #t) collision-structure)
+;  #:depends '(compute-collision-structure)
 ;  #:map (lambda (e) (rotate90 e)))
 ;
 ;(define-system move-down    
 ;  #:archetype ActiveTetromino
-;  #:on clock-tick (eq? can-move-down? #t)
+;  #:on '(clock-tick (eq? can-move-down? #t))
 ;  #:map (lambda (e)
 ;    (set! e.Position.y (sub1 e.Position.y))))
 ;
 ;(define-system move-right    
 ;  #:archetype ActiveTetromino
-;  #:on (eq? can-move-right? #t)
+;  #:on '((eq? can-move-right? #t))
 ;  #:map (lambda (e)
 ;    (set! e.Position.x (add1 e.Position.x))))
 ;
 ;(define-system move-left    
 ;  #:archetype ActiveTetromino
-;  #:on (eq? can-move-left? #t)
+;  #:on '((eq? can-move-left? #t))
 ;  #:map (lambda (e)
 ;    (set! e.Position.x (sub1 e.Position.x))))
 ;
 ;(define-system can-rotate-ccw?    
 ;  #:archetype ActiveTetromino
-;  #:on collision-structure
-;  #:out can-rotate-ccw?
-;  #:depends compute-collision-structure
+;  #:on '(collision-structure)
+;  #:out '(can-rotate-ccw?)
+;  #:depends '(compute-collision-structure)
 ;  #:map (lambda (e) (valid-ccw? e collision-structure)))
 ;
 ;(define-system can-rotate-cw?    
 ;  #:archetype ActiveTetromino
-;  #:on collision-structure
-;  #:out can-rotate-cw?
-;  #:depends compute-collision-structure
+;  #:on '(collision-structure)
+;  #:out '(can-rotate-cw?)
+;  #:depends '(compute-collision-structure)
 ;  #:map (lambda (e) (valid-cw? e collision-structure)))
 ;
 ;(define-system can-move-down?    
 ;  #:archetype ActiveTetromino
-;  #:on collision-structure
-;  #:out can-move-down?
-;  #:depends compute-collision-structure
+;  #:on '(collision-structure)
+;  #:out '(can-move-down?)
+;  #:depends '(compute-collision-structure)
 ;  #:map (lambda (e) (vacant-down? e collision-structure)))
 ;
 ;
 ;(define-system can-move-right?    
 ;  #:archetype ActiveTetromino
-;  #:on collision-structure
-;  #:out can-move-right?
-;  #:depends compute-collision-structure
+;  #:on '(collision-structure)
+;  #:out '(can-move-right?)
+;  #:depends '(compute-collision-structure)
 ;  #:map (lambda (e) (vacant-right? e collision-structure)))
 ;
 ;
 ;(define-system can-move-left?    
 ;  #:archetype ActiveTetromino
-;  #:on collision-structure
-;  #:out can-move-left?
-;  #:depends compute-collision-structure
+;  #:on '(collision-structure)
+;  #:out '(can-move-left?)
+;  #:depends '(compute-collision-structure)
 ;  #:map (lambda (e) (vacant-left? e collision-structure)))
 
-
-;; XXX Worlds
+; XXX Worlds
