cf1841a75d00fa180671d53a92073d65ff9a579f
Chike Abuah <abuahchu@gmail.com>
2018-07-22 13:11:41 -0400

    define-system syntax and identifier bindings

--- examples/tetris/recess-graph.rkt
+++ examples/tetris/recess-graph.rkt
@@ -42,9 +42,46 @@
        #;#''(name (~? pred '()) (~? body '()))
        #'(define name (create-event 'name)))]))
 
+;;; define-system syntax and identifier bindings
+
+#;(define-system system:id
+  ;; a specification of the 'type' of entities that are needed
+  #:archetype archetype:id
+  ;; initial state of the system
+  #:init initial-state:expr
+  ;; Runs before the iteration to potentially update the state
+  ;; and gather some data for this iteration
+  ;; `system-state` is bound to the system's current state in pre-body
+  ;; `in-events` is bound to the system's input events in pre-body
+  #:pre pre-body:expr
+  ;; generates the final state of the system for the iteration and then produces output
+  ;; `system-state` is bound to the system's current state in post-body 
+  #:post post-body:expr
+  ;; decides if the query should be run (post will run anyway)
+  ;; `system-state` is bound to the system's current state in pred-expr
+  #:enabled pred-expr:expr
+  ;; a static specification of which events are inputs
+  #:on list-of-events:expr
+  ;; a static specification of which events may be output
+  #:out list-of-events:expr
+  ;; processes a single entity
+  ;; `system-state` is bound to the system's current state in map-body  
+  ;; `e` is bound to the entity in map-body
+  #:map map-body:expr
+  ;; combines the results of many entities
+  ;; `system-state` is bound to the system's current state in reduce-body
+  ;; `e` is bound to the entity in reduce-body  
+  #reduce reduce-body:expr
+  ;; the unit of the reduction for the iteration
+  ;; `system-state` is bound to the system's current state in zero-expr
+  #:zero zero-expr:expr
+  )
+
+
 ;; register a system in the graph and have it print out the graph's
 ;; structure on each call
 ;; last graph printed is the full picture
+
 (define-syntax (define-system stx)
   (syntax-parse stx
     [(_ system-name
@@ -75,6 +112,6 @@
 (define-syntax (unless-defined stx)
   (syntax-case stx ()
     [(_ id then)
-       (cond
-         [(identifier-binding #'id) #'id]
-         [else #'then])]))
+     (cond
+       [(identifier-binding #'id) #'id]
+       [else #'then])]))
