aa9a9e3ba4279f048b8b493b70af85e77f7e8015
Chike Abuah <abuahchu@gmail.com>
2018-09-01 17:09:13 -0400

    factor out big bang step function

--- main.rkt
+++ main.rkt
@@ -7,8 +7,7 @@
    racket/syntax
    racket/match)
   graph
-  2htdp/universe
-  2htdp/image
+  recess/big-bang-step
   racket/syntax
   racket/match
   racket/generic
@@ -147,9 +146,6 @@
 (define current-events (make-parameter (make-immutable-hasheq)))
 (define start-time (make-parameter (current-seconds)))
 
-;; adapt recess to use big bang
-(struct big-bang-recess-world (pending-events recess-state last-output) #:transparent)
-
 ;; initialize, iterate, terminate at some point
 ;; create a topological ordering of the recess
 ;; graph and execute the nodes in that order
@@ -172,51 +168,7 @@
               (add-to-graph sys (system-in sys) (list) (world-dependency-graph (current-world))))
             systems)
            ;; iterate through the graph until the world's termination conditions are fulfilled
-           (big-bang (big-bang-recess-world (make-immutable-hasheq) #f #f)
-             (on-tick (big-bang-step-world) 1)
-             (to-draw big-bang-draw-recess)
-             (on-key big-bang-recess-key)
-             (on-mouse big-bang-recess-mouse)
-             (stop-when (big-bang-stop-condition (λ () (and stop-expr ...)))))))]))
-
-;; `on-key` and `on-mouse` events record something inside a custom made world struct
-(define (big-bang-recess-key w key-event)
-  (define events-so-far (big-bang-recess-world-pending-events w))
-  (struct-copy big-bang-recess-world w
-               [pending-events (hash-set events-so-far 'key/e key-event)]))
-
-(define (big-bang-recess-mouse w x y mouse-event)
-  (define events-so-far (big-bang-recess-world-pending-events w))
-  (struct-copy big-bang-recess-world w
-               [pending-events (hash-set events-so-far 'mouse/e mouse-event)]))
-
-;; then `on-tick` takes those events, plus a freshly generated clock event and runs the
-;; simulation, records the output in the world struct
-(define (big-bang-step-world)
-  (λ (w)
-    ;; sync up with new things that have happened
-    ;; right now this means merging the pending events into the current events
-    (define events-so-far
-      (hash-set
-       (big-bang-recess-world-pending-events w)
-       'clock/e
-       (- (current-seconds) (start-time))))
-    (current-events (hash-union (current-events) events-so-far #:combine (λ (old new) new)))
-    ;;then step
-    (displayln "executing recess graph...")
-    (step-world)
-    ;; reset pending events and produce output
-    ;; need to reset sink events too
-    (struct-copy big-bang-recess-world w
-                 [pending-events (make-immutable-hasheq)]
-                 [last-output (list)])))
-
-;; then `on-draw` will just pulls out the recess sink output
-(define (big-bang-draw-recess w)
-  (text "Hello" 24 "olive"))
-
-(define (big-bang-stop-condition stop-func)
-  (λ (w) (stop-func)))
+           (big-bang-step start-time (λ () (and stop-expr ...)) current-events step-world)))]))
 
 ;; do a single iteration of a world graph
 (define (step-world)
