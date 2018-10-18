789acdd3e6483486ef16720916ee6364f3878c0b
Ben Greenman <benjaminlgreenman@gmail.com>
2018-10-18 17:13:19 -0400

    ** add info file

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "recess")
+(define deps '("base" "draw-lib" "graph" "gui-lib" "htdp-lib" "lux" "mode-lambda" "pict-lib" "raart" "struct-define"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define pkg-desc "TBA")
+(define version "0.1")
+(define pkg-authors '(chike))
+(define compile-omit-paths '("examples"))
