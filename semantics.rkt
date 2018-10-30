6121d44ec83863ca352805fee4de26318a9e4788
Chike Abuah <abuahchu@gmail.com>
2018-10-30 17:45:51 -0500

    start writing semantics

---
+++ semantics.rkt
@@ -0,0 +1,32 @@
+#lang racket
+(require redex)
+
+(define-language ECS
+  (index ::= number)
+  (identity ::= (symbol index))
+  (component ::= (identity number))
+  (v ::=
+     x
+     number
+     component)
+  (x ::= variable-not-otherwise-mentioned)
+  (e ::=
+     v
+     (component-ref number)
+     (+ e ...))
+  (E ::= hole
+     (+ v ... E e ...))
+  (entity ::= (identity (component ...)))
+  (entity-e ::= (identity (e ...)))
+  (entity-v ::= (identity (v ...)))
+  (entity-E ::= (identity (v ... E e ...)))
+  (ent-map-e ::=
+           (set-entity entity index e)
+           (ent-map ...))
+  (ent-map-v ::=
+           (set-entity entity index v)
+           (ent-map ...))
+  (ent-map-E ::=
+           (set-entity entity index E)
+           (ent-map-E ...)))
+
